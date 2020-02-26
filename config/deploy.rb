1dfc4542651f83fe3e11f53628309addc297f9d1
justin3724 <hi_justin@hotmail.com>
2020-02-26 22:39:16 +1100

    db:schema

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,3 +40,22 @@ set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+namespace :deploy do
+  namespace :db do
+    desc "Load the database schema if needed"
+    task load: [:set_rails_env] do
+      on primary :db do
+        if not test(%Q[[ -e "#{shared_path.join(".schema_loaded")}" ]])
+          within release_path do
+            with rails_env: fetch(:rails_env) do
+              execute :rake, "db:schema:load"
+              execute :touch, shared_path.join(".schema_loaded")
+            end
+          end
+        end
+      end
+    end
+  end
+
+  before "deploy:db:migrate", "deploy:db:load"
+end
