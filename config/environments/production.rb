ffe1cdb3767e76d6916e45c2bd0820888f09b10b
justin3724 <hi_justin@hotmail.com>
2020-03-05 18:39:35 +1100

    fix redis_cache 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,8 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-  #config.cache_store = :redis_store, "redis://localhost:6379/0/cache", { expires_in: 90.minutes }
+  cache_server = "redis://localhost:6379/0/cache"
+  config.cache_store = :redis_cache_store, { url: cache_server }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
