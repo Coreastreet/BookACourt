74cd043f49099f08f9a6cb56641943b0497ba5ac
justin3724 <hi_justin@hotmail.com>
2020-03-29 18:02:49 +1100

    missing secret key2

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :deploy_to, "/home/deploy/rails/#{fetch :application}"
 
 # Default value for :pty is false
 # set :pty, true
-set :linked_files, %w{config/master.key}
+# set :linked_files, %w{config/master.key}
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
 
