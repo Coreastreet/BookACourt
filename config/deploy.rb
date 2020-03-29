6c97071f30e9302e1bbc833141136ca33d7d7597
justin3724 <hi_justin@hotmail.com>
2020-03-29 19:59:03 +1100

    change to justin

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :bundle_without, %w{test}.join(' ')
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/deploy/rails/#{fetch :application}"
+set :deploy_to, "/home/justin/rails/#{fetch :application}"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -37,7 +37,8 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bund
 
 # Default value for keep_releases is 5
 set :keep_releases, 3
-
+set :pg_database, 'BookACourt'
+set :pg_username, 'justin'
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 namespace :deploy do
