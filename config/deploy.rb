2716ca58d55817f59478a2a7f94c6dcaf1e5eab1
justin3724 <hi_justin@hotmail.com>
2020-03-30 19:05:16 +1100

    reinstate old database

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, "/home/deploy/rails/#{fetch :application}"
 # set :pty, true
 # set :linked_files, %w{config/master.key}
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", ".bundle", "public/system", "public/uploads"
