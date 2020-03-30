1b738d4516cced9331212acc54125560e60720c6
justin3724 <hi_justin@hotmail.com>
2020-03-30 18:27:54 +1100

    append database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,12 +23,11 @@ set :deploy_to, "/home/deploy/rails/#{fetch :application}"
 # set :pty, true
 # set :linked_files, %w{config/master.key}
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", ".bundle", "public/system", "public/uploads"
-set :linked_files, %w{config/database.yml}
-
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
