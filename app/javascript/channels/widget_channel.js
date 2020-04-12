99a58894ed1963279c7153a80e5243c16fa7b796
justin3724 <hi_justin@hotmail.com>
2020-04-13 07:15:09 +1000

    remove function test ws

--- app/javascript/channels/widget_channel.js
+++ app/javascript/channels/widget_channel.js
@@ -14,35 +14,3 @@ consumer.subscriptions.create("WidgetChannel", {
       console.log(`incoming data, ${data}`);
   }
 });
-
-function WebSocketTest() {
-
-           if ("WebSocket" in window) {
-              alert("WebSocket is supported by your Browser!");
-
-              // Let us open a web socket
-              const ws = new WebSocket('wss://api.probit.com/api/exchange/v1/ws');
-
-              ws.onopen = () => {
-                const msg = {
-                  type: 'subscribe',
-                  identifier: '{\"channel\":\"WidgetChannel\"}',
-                };
-                ws.send(JSON.stringify(msg));
-              };
-
-              ws.onmessage = (event) => {
-                console.log(event.data);
-                alert(event.data);
-              };
-
-              ws.onclose = function() {
-                 // websocket is closed.
-                 alert("Connection is closed...");
-              };
-           } else {
-
-              // The browser doesn't support WebSocket
-              alert("WebSocket NOT supported by your Browser!");
-           }
-}
