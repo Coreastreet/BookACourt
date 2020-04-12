c32d2612920193b6db69f9cce147ef4665fe3be8
justin3724 <hi_justin@hotmail.com>
2020-04-13 08:06:05 +1000

    remove redis

--- app/javascript/channels/widget_channel.js
+++ app/javascript/channels/widget_channel.js
@@ -3,7 +3,7 @@ import consumer from "./consumer"
 consumer.subscriptions.create("WidgetChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
-    console.log(`connected!`);
+    console.log("connected");
   },
 
   disconnected() {
