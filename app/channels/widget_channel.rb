d11325b55ae99372ec551072bdb0c965ed6f4c66
justin3724 <hi_justin@hotmail.com>
2020-04-13 10:06:52 +1000

    widgets

--- app/channels/widget_channel.rb
+++ app/channels/widget_channel.rb
@@ -1,10 +1,18 @@
 class WidgetChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
+    stream_from "updates_#{current_user}"
     Rails.logger.info("subscribed to widget channel")
   end
 
+  def update(data)
+    ActionCable.server.broadcast "updates_#{data.current_user}", {
+      message: "Another booking has been made!",
+    }
+  end
+
   def unsubscribed
+    Rails.logger.info("unsubscribed from widget channel")
     # Any cleanup needed when channel is unsubscribed
   end
 end
