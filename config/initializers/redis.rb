2b11f4bce74a1a1341a5f9410f38ac505ce50f2b
justin3724 <hi_justin@hotmail.com>
2020-04-14 21:25:39 +1000

    change redis url

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,5 +3,5 @@
 
 # The constant below will represent ONE connection, present globally in models, controllers, views etc for the instance. No need to do Redis.new everytime
 
-Redis.current = Redis.new(host: "localhost")
+Redis.current = Redis.new(url: "redis://:2XIXEM10cIfxd2A9XWEAoadSvevhTDck@redis-17897.c1.ap-southeast-1-1.ec2.cloud.redislabs.com:17897")
 $redis = Redis.current
