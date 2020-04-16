3848a3fd2e334bdd9d902fad1f86ebfb49810727
justin3724 <hi_justin@hotmail.com>
2020-04-16 10:14:04 +1000

    move back to home page

--- app/controllers/api/v1/sports_centres_controller.rb
+++ app/controllers/api/v1/sports_centres_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::SportsCentresController < Api::V1::ApiController
-  include ActionController::Live
 
   CHARS = ('0'..'9').to_a + ('A'..'Z').to_a + ('a'..'z').to_a
 
@@ -16,25 +15,6 @@ class Api::V1::SportsCentresController < Api::V1::ApiController
         redirect_to login_path
     end
 
-  end
-
-  def live_update
-    response.headers['Content-Type'] = 'text/event-stream'
-    sse = SSE.new(response.stream, retry: 5000)
-    id = 0
-    sports_centre = SportsCentre.find(params[:id])
-    begin
-        sports_centre.on_bookings_change do |sports_centre_id|
-          #if (status == "new_booking")
-            @bookings = sports_centre.bookings.to_json.html_safe
-            sse.write({ bookings: @bookings }, id: id, event: "live_update")
-            id = id + 1
-            #sse.write({ name: status }, id: 10, event: "demo_update")
-        end
-    rescue ClientDisconnected
-    ensure
-      sse.close
-    end
   end
 
   def payment_nudge
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  include ActionController::Live
 
   def index
     # console
@@ -8,6 +9,25 @@ class HomeController < ApplicationController
   end
 
   def demo
+  end
+
+  def live_update
+    response.headers['Content-Type'] = 'text/event-stream'
+    sse = SSE.new(response.stream, retry: 5000)
+    id = 0
+    sports_centre = SportsCentre.find(params[:id])
+    begin
+        sports_centre.on_bookings_change do |sports_centre_id|
+          #if (status == "new_booking")
+            @bookings = sports_centre.bookings.to_json.html_safe
+            sse.write({ bookings: @bookings }, id: id, event: "live_update")
+            id = id + 1
+            #sse.write({ name: status }, id: 10, event: "demo_update")
+        end
+    rescue ClientDisconnected
+    ensure
+      sse.close
+    end
   end
 
 end
