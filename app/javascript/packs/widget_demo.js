9d78ca507d800b5e060122be7d8f53ee48619c2f
justin3724 <hi_justin@hotmail.com>
2020-04-22 18:51:31 +1000

    fix 114 static

--- app/javascript/packs/widget_demo.js
+++ app/javascript/packs/widget_demo.js
@@ -1261,8 +1261,8 @@ $(document).on('turbolinks:load', function ()  {
                                     alert("Your preferred booking time is no longer available. Please try again.");
                                     return false;
                                 }
-                                var request = makeCORSRequest(`http://localhost:3000/api/v1/sports_centres/114/bookings/reserve`, "POST");
-                                //var request = makeCORSRequest(`https://weball.com.au/api/v1/sports_centres/${sportsCentreId}/bookings/reserve`, "POST");
+                                //var request = makeCORSRequest(`http://localhost:3000/api/v1/sports_centres/114/bookings/reserve`, "POST");
+                                var request = makeCORSRequest(`https://weball.com.au/api/v1/sports_centres/${sportsCentreId}/bookings/reserve`, "POST");
                                 //*****var request = makeCORSRequest(`https://weball.com.au/api/v1/sports_centres/${sportsCentreId}/bookings/initiate`, "POST");
                                 request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
                                 // store reservation time so you identify the bookings the customer personally makes.
